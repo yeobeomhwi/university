7b5a65d0962740558e3892b842da41465a8c63d0
yeobeomhwi <103626708+yeobeomhwi@users.noreply.github.com>
2023-03-20 16:19:11 +0900

    모바일프로그래밍 3주차

--- JAVA/Project 2/src/App.java
+++ JAVA/Project 2/src/App.java
@@ -1,5 +1,43 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class App extends JFrame {
+
+    JButton b1,b2;
+    JPanel p1,p2;
+    
+    public App(){
+     Container cp=this.getContentPane();
+     p1=new JPanel(); p2=new JPanel();
+     cp.add(p1, "North"); cp.add(p2, "Center");
+     b1=new JButton("노란색");
+     b2=new JButton("핑크색");
+     p1.add(b1); p1.add(b2);
+     p2.setBackground(Color.white);
+     
+     b1.addActionListener(new ActionListener(){
+
+      public void actionPerformed(ActionEvent e){
+       p2.setBackground(Color.yellow);
+      }
+      
+     });
+     
+     b2.addActionListener(new ActionListener(){
+      
+      public void actionPerformed(ActionEvent e){
+       p2.setBackground(Color.pink);
+      }
+     });
+     
+     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-}
+    
+    public static void main(String[] args) {
+     App my=new App();
+     my.setSize(500, 500);
+     my.setVisible(true);
+     
+    }
+   }
