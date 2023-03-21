bb5f73ff3c5e689b65ca14bfd25ec2d23bd0609f
yeobeomhwi <103626708+yeobeomhwi@users.noreply.github.com>
2023-03-21 17:14:30 +0900

    23-03-21-cpp

---
+++ CPP/TEST3-10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct StructCircle
+{
+    private:
+        int radius;
+    public:
+        StructCircle(int r){ radius = r; }
+        double getArea();
+};
+
+double StructCircle::getArea()
+{
+    return 3.14*radius*radius;
+}
+
+int main()
+{
+    StructCircle waffle(3);
+    cout << "¸éÀûÀº " <<waffle.getArea();
+}
---
+++ CPP/TEST3-11/Calculator.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+private:
+    /* data */
+public:
+    void run();
+};
+
+#endif
