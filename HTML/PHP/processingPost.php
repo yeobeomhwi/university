e7f9b2fd82220b4611113ba2d64d89aa6c7b45a3
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2023-03-28 16:08:43 +0900

    23-03-27-HTML

---
+++ HTML/PHP/processingPost.php
@@ -0,0 +1,27 @@
+﻿<doctype html>
+<html> <title> POST 전송방식 </title>
+<head>
+</head>
+<body>
+<?PHP 
+  extract(array_merge($_GET, $_POST)); 
+  echo " <h2> 성적처리 결과 </h2><hr><hr>" ; 
+  $total = $middle + $last; 
+  $avg = $total / 2;  
+  echo "이름 : $name ($major) <br> "; 
+  echo "중간 : $middle <br> "; 
+  echo "기말 : $last <br> ";  
+  echo "총점 : $total <br> "; 
+  echo "평균 : $avg <br> ";  
+?> 
+</body>
+</html>
+
+
+
+
+
+
+
+
+
