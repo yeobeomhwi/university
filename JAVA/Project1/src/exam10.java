2cb7a71b8c53468abe68928d0b3a8d236d8f286d
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2023-04-20 20:47:20 +0900

    .

--- JAVA/Project1/src/exam10.java
+++ JAVA/Project1/src/exam10.java
@@ -1,11 +1,20 @@
-
+import java.util.ArrayList;
 
 public class exam10 {
-    public static void main(String args[]){
-        Tiger tiger1 = new Tiger();
-        Eagle eagle1 = new Eagle();
 
-        tiger1.move();
-        eagle1.move();
+    public static void main(String args[]){
+        myClass c1 = new myClass(10);
+        main(args);
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+        numList.add(1);
+        numList.add(2);
+        numList.add(3);
+    
+        int hap = 0;
+        for(int i = 0; i < numList.size(); i++){
+            hap += numList.get(i);
+        }
+        System.out.println(hap);
     }
+    
 }
